9402a613f1b531c6c5f44f25c834cec3f1afacc9
floprete <floprete@ivytech.edu>
2023-07-14 10:31:51 -0500

    M05 Tutorial - Intro to Express and Full Stack development

---
+++ node-crash-course/app.js
@@ -0,0 +1,34 @@
+const express =require('express');
+
+//express app
+const app=express();
+
+// register view engine
+app.set('view engine', 'ejs');
+
+//listen for requests
+app.listen(3000);
+
+app.get('/', (req, res) => {
+    const blogs = [
+        {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+        {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+];
+   res.render('index', { title: 'Home', blogs });
+});
+
+app.get('/about', (req, res) => {
+    res.render('about', { title: 'About Blog Ninja' });
+  });
+
+  app.get('/create', (req, res) => {
+    res.render('create', { title: 'Create New Blog' });
+  });  
+
+
+//404 page
+
+app.use((req, res) => {
+    res.status(404).render('404', { title: '404 Page Not Found' });
+  });
