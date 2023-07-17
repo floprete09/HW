ae7dffab8dee94ac45ee5846fc26d0ddc48f8ddd
floprete <floprete@ivytech.edu>
2023-07-16 19:00:16 -0500

    works

---
+++ NINJA/node-crash-course-lesson-2/app.js
@@ -0,0 +1,15 @@
+const express =require('express');
+
+//express app
+const app=express();
+
+//listen for requests
+app.listen(3000);
+
+app.get('/', (req, res)=>{
+    res.send('<p>Home page</p>');
+});
+
+app.get('/about', (req, res)=>{
+    res.send('<p>About page</p>');
+});
